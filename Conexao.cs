b8887832adee273e2935a55cb9ca505f9f44e6c3
Papaizao <90474758+Papaizao@users.noreply.github.com>
2022-09-01 10:46:56 -0300

    Add files via upload

---
+++ Conexao.cs
@@ -0,0 +1,102 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MauiApp1
+{
+    public class Conexao
+    {
+        private string nome;
+        private string senha;
+        private string registro;
+
+        public String getRegistro()
+        {
+            return this.registro;
+        }
+        public void Consulta()
+        {
+           
+            MySqlConnection objcon = new MySqlConnection("server=localhost;port=3306;User Id=root;database=dumdum; password=Senbonzakur@1");
+            objcon.Open();
+
+            MySqlCommand objCmd = new MySqlCommand("select * from crud", objcon);
+            
+            MySqlDataReader dr;
+            dr = objCmd.ExecuteReader();
+            dr.Read();
+            String registro = "";
+            if(registro != "")
+            {
+                this.registro = registro;
+            }
+            objcon.Close();
+        }
+        public void Deletar(String nome)
+        {
+            this.nome = nome;
+
+            
+                MySqlConnection objcon = new MySqlConnection("server=localhost;port=3306;User Id=root;database=dumdum; password=Senbonzakur@1");
+                objcon.Open();
+
+                MySqlCommand objCmd = new MySqlCommand("select nome from crud where nome = ?", objcon);
+                objCmd.Parameters.Add("@nome", MySqlDbType.VarChar, 40).Value = this.nome;
+
+                MySqlDataReader dr;
+                dr = objCmd.ExecuteReader();
+                dr.Read();
+                String b = dr.GetString(0);
+                objcon.Close();
+                if (b.Equals(this.nome))
+                {
+                    objcon.Open();
+
+                    MySqlCommand objcmd = new MySqlCommand("delete from crud where nome = ?", objcon);
+                    objcmd.Parameters.Add("@nome", MySqlDbType.VarChar, 40).Value = this.nome;
+                    
+                    objcmd.ExecuteNonQuery();
+
+                }
+
+                objcon.Close();
+        }
+        public void Adicionar(string nome, string senha)
+        {
+                this.nome = nome;
+                this.senha = senha;
+
+           
+                MySqlConnection objcon = new MySqlConnection("server=localhost;port=3306;User Id=root;database=dumdum; password=Senbonzakur@1");
+                objcon.Open();
+
+                MySqlCommand objCmd = new MySqlCommand("insert into crud(nome, senha) values(?, ?)", objcon);
+                objCmd.Parameters.Add("@nome", MySqlDbType.VarChar, 40).Value = this.nome;
+                objCmd.Parameters.Add("@senha", MySqlDbType.VarChar, 40).Value = this.senha;
+
+
+                objCmd.ExecuteNonQuery();
+           
+        }
+        public void Atualizar(string nome,string senha)
+        {
+            this.nome = nome;
+            this.senha = senha;
+
+            MySqlConnection objcon = new MySqlConnection("server=localhost;port=3306;User Id=root;database=dumdum; password=Senbonzakur@1");
+            objcon.Open();
+
+
+            MySqlCommand objCMD = new MySqlCommand("update crud set senha = ? where nome = ?", objcon);
+            objCMD.Parameters.Add("@senha", MySqlDbType.VarChar, 40).Value = this.senha;
+            objCMD.Parameters.Add("@nome", MySqlDbType.VarChar, 40).Value = this.nome;
+
+            objCMD.ExecuteNonQuery();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿
+using MySqlConnector;
+using System.Data;
+using System.Data.SqlTypes;
+namespace MauiApp1;
+
+
+public partial class MainPage : ContentPage
+{
+	
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+    public void Deletar(object sender, EventArgs e)
+    {
+        try
+        {
+            string s = Name.Text;
+            Conexao c1 = new Conexao();
+            c1.Deletar(s);
+            msg.Text = "Deletado com sucesso";
+        }
+        catch(Exception ex)
+        {
+            
+            msg.Text ="Erro" + ex;
+        }
+    }
+    public void Adicionar(object sender, EventArgs e)
+    {
+        string nome = Name.Text;
+        string senha = Senha.Text;
+        if (nome != "" && senha != "")
+        {
+            try
+            {
+                Conexao c1 = new Conexao();
+                c1.Adicionar(nome, senha);
+                msg.Text = "Adicionado " + nome + " com sucesso";
+            }
+            catch (Exception ex)
+            {
+                msg.Text = "Erro" + ex;
+            }
+        }
+        else
+        {
+            msg.Text = "Preencha os campos em branco";
+        }
+    }
+
+   /* public void Consulta(object sender, EventArgs e)
+    {
+        
+        Conexao c1 = new Conexao();
+        c1.Consulta();
+        c1.getRegistro();        
+    }*/
+    public void Atualizar(object sender, EventArgs e)
+    {
+        string nome = Name.Text;
+        string senha = Senha.Text;
+        try
+        {
+            Conexao c1 = new Conexao();
+            c1.Atualizar(nome, senha);
+            msg.Text = "Atualizado o campo senha com sucesso";
+
+        }
+        catch(Exception ex)
+        {
+            msg.Text = "Erro" + ex;
+        }
+    }
+   
+    public void Limpar(object sender, EventArgs e)
+    {
+        Name.Text = default;
+        Senha.Text = default;
+        msg.Text = default;
+    }
+
+}
